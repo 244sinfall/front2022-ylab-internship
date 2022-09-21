0010f8bebebb839aacfd065263b36620d8ceac1f
Dima <dimafilin6@icloud.com>
2022-09-21 23:39:05 +0700

    фиксы

--- src/components/elements/custom-select/index.js
+++ src/components/elements/custom-select/index.js
@@ -21,9 +21,9 @@ function CustomSelect(props) {
   }, [opened])
   // Кто вообще пользуется этими табами................
   return (
-    <div className={cn()} onKeyDown={e => e.code === "Escape" && opened && setOpened(false)}>
+    <div autofocus className={cn()} onKeyDown={e => e.code === "Escape" && opened && setOpened(false)}>
         <span className={cn('selector')} onClick={() => setOpened(!opened)} onKeyDown={e => e.code === "Space" && !opened && setOpened(true)} tabIndex="0" role="listbox">
-          <CustomSelectListItem name={selectedItem.name} code={selectedItem.code} />
+          <CustomSelectListItem name={selectedItem.name} code={selectedItem.code} tabIndex="-1" />
           <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M0.410765 0.910734C0.736202 0.585297 1.26384 0.585297 1.58928 0.910734L6.00002 5.32148L10.4108 0.910734C10.7362 0.585297 11.2638 0.585297 11.5893 0.910734C11.9147 1.23617 11.9147 1.76381 11.5893 2.08925L6.58928 7.08925C6.26384 7.41468 5.7362 7.41468 5.41077 7.08925L0.410765 2.08925C0.0853278 1.76381 0.0853278 1.23617 0.410765 0.910734Z" fill="black" />
           </svg>
--- src/components/elements/custom-select/list-item/index.js
+++ src/components/elements/custom-select/list-item/index.js
@@ -6,7 +6,7 @@ import './styles.css'
 function CustomSelectListItem(props) {
   const cn = bem('CustomSelectListItem');
   return (
-    <span className={cn()} onClick={props.onClick} onKeyDown={e => e.code === "Enter" && props.onClick()} role="option" tabIndex="0">
+    <span aria-role className={cn()} onClick={props.onClick} onKeyDown={e => e.code === "Enter" && props.onClick()} role="option" tabIndex={props.tabIndex}>
       <p aria-hidden="true" className={cn('code')}>{props.code ?? props.name.substring(0, 2).toUpperCase()}</p>
       <p className={cn('name')}>{props.name}</p>
     </span>
@@ -17,11 +17,13 @@ CustomSelectListItem.propTypes = {
   name: propTypes.string.isRequired,
   onClick: propTypes.func,
   code: propTypes.string,
+  tabIndex: propTypes.string
  
 }
 
 CustomSelectListItem.defaultProps = {
-  onClick: () => {}
+  onClick: () => {},
+  tabIndex: 0
 }
 
 export default React.memo(CustomSelectListItem);
