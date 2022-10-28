513d785aaec326a780b012da14f8684bc6872d4a
Dima <dimafilin6@icloud.com>
2022-10-28 12:33:29 +0700

    Задание с канвасом полностью выполнено

--- src/components/canvas/canvas-options/index.js
+++ src/components/canvas/canvas-options/index.js
@@ -10,7 +10,7 @@ const CanvasOptions = props => {
     <div className={cn()}>
       <span className={cn('draw')}>
         <CustomSelect value={props.selectedShape} onChange={props.onShapeSelectionChange} options={props.drawOptions}/>
-        <label htmlFor="shouldFill">Заполнить цветом?</label>
+        <label htmlFor="shouldFill">{props.t('canvas.shouldFill')}</label>
         <input id="shouldFill" type={'checkbox'} onChange={props.onFillCheck}/>
         <button onClick={props.onDraw} disabled={props.drawDisabled}>{props.t('canvas.draw')}</button>
       </span>
